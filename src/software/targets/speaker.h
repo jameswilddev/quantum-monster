ea900b0cd8c61c1ecdafddd2e23ec65898ae20d2
jameswilddev <jameswilddev@outlook.com>
2022-01-06 22:18:03 +0000

    Add missing variable.

--- src/software/targets/speaker.h
+++ src/software/targets/speaker.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 
 /**
- * @brief The state of the speaker.  Changes are applied after qm_refresh() returns.  The default value is QM_SPEAKER_STATE_MUTED.  See qm_speaker_state and QM_SPEAKER_STATE_*.
+ * @brief A state of the speaker; see qm_speaker_state and QM_SPEAKER_STATE_*.
  */
 typedef uint8_t qm_speaker_state_t;
 
@@ -13,5 +13,10 @@ typedef uint8_t qm_speaker_state_t;
  * @brief The speaker is to stop producing sound.
  */
 #define QM_SPEAKER_STATE_MUTED 0
+
+/**
+ * @brief The state of the speaker.  Changes are applied after qm_refresh() returns.  The default value is QM_SPEAKER_STATE_MUTED.  See QM_SPEAKER_STATE_*.
+ */
+extern qm_speaker_state_t qm_speaker_state;
 
 #endif
