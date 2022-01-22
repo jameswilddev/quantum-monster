a24927472fefb526774e10bb80ec31e43a41893d
jameswilddev <jameswilddev@outlook.com>
2022-01-22 21:21:03 +0000

    Work-in-progress sprite conversion.

--- makefile
+++ makefile
@@ -1,30 +1,174 @@
+SUBMODULE_H_FILES = $(shell find submodules -name "*.h")
+
+PREVIOUSLY_GENERATED_FILES = $(shell find src/software -name "*.*.h") $(shell find src/software -name "*.*.c") $(shell find src/software -name "*.ase.png") $(shell find src/software -name "*.aseprite.png")
+
+TOOL_CC = $(CC)
+TOOL_CFLAGS = -Wall -O2
+TOOL_LDFLAGS = -lm
+TOOL_C_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/tools -name "*.c"))
+TOOL_NAMES = $(TOOL_C_FILES:src/software/tools/%.c=%)
+TOOL_DISTRIBUTIBLES = $(TOOL_NAMES:%=dist/tools/%)
+
+ASEPRITE_FILES = $(shell find src/software -name "*.ase") $(shell find src/software -name "*.aseprite")
+CONVERTED_ASEPRITE_FILES = $(ASEPRITE_FILES:%=%.png)
+IMAGE_FILES = $(shell find src/software -name "*.jpg") $(shell find src/software -name "*.jpeg") $(shell find src/software -name "*.png") $(shell find src/software -name "*.tga") $(shell find src/software -name "*.bmp") $(shell find src/software -name "*.psd") $(shell find src/software -name "*.psd") $(shell find src/software -name "*.gif") $(shell find src/software -name "*.hdr") $(shell find src/software -name "*.pic") $(shell find src/software -name "*.pnm")
+NON_GENERATED_IMAGE_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(IMAGE_FILES))
+IMAGE_FILES_TO_CONVERT = $(CONVERTED_ASEPRITE_FILES) $(NON_GENERATED_IMAGE_FILES)
+CONVERTED_IMAGE_H_FILES = $(IMAGE_FILES_TO_CONVERT:%=%.h)
+CONVERTED_IMAGE_C_FILES = $(IMAGE_FILES_TO_CONVERT:%=%.c)
+
 APPLICATION_DIRECTORIES = $(shell find src/software/applications/* -type d)
 APPLICATION_NAMES = $(APPLICATION_DIRECTORIES:src/software/applications/%=%)
-H_FILES = $(shell find src/software -name "*.h")
-LIBRARY_C_FILES = $(shell find src/software/library -name "*.c")
-APPLICATION_C_FILES = $(shell find src/software/applications -name "*.c")
+APPLICATION_H_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/applications -name "*.h"))
+APPLICATION_C_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/applications -name "*.c"))
+LIBRARY_H_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/library -name "*.h"))
+LIBRARY_C_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/library -name "*.c"))
+TARGET_H_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/targets -name "*.h"))
 
 AVR_CC = avr-gcc
-AVR_CFLAGS = -Wall -O2 -mmcu=atmega328p -DQM_STATIC_DATA=PROGMEM -DQM_DISPLAY_WIDTH=48 -DQM_DISPLAY_HEIGHT=84
+AVR_CFLAGS = -Wall -O2 -mmcu=atmega328p -D'QM_STATIC_DATA=__attribute__((__progmem__))' -DQM_DISPLAY_WIDTH=48 -DQM_DISPLAY_HEIGHT=84
 AVR_LDFLAGS =
 AVR_DISTRIBUTABLES = $(addsuffix .hex,$(addprefix dist/avr/,$(APPLICATION_NAMES)))
-AVR_C_FILES = $(shell find src/software/targets/avr -name "*.c")
-AVR_TARGET_AND_LIBRARY_O_FILES = $(AVR_C_FILES:src/software/%.c=obj/avr/compiled/%.o) $(LIBRARY_C_FILES:src/software/%.c=obj/avr/compiled/%.o)
+AVR_C_FILES = $(filter-out $(PREVIOUSLY_GENERATED_FILES),$(shell find src/software/targets/avr -name "*.c"))
+AVR_TARGET_O_FILES = $(AVR_C_FILES:src/software/%.c=obj/avr/compiled/%.o)
+AVR_LIBRARY_O_FILES = $(LIBRARY_C_FILES:src/software/%.c=obj/avr/compiled/%.o)
 AVR_APPLICATION_O_FILES = $(APPLICATION_C_FILES:src/software/%.c=obj/avr/compiled/%.o)
+AVR_IMAGE_O_FILES = $(IMAGE_FILES_TO_CONVERT:src/software/%=obj/avr/compiled/%.o)
 
-all: $(AVR_DISTRIBUTABLES)
+all: $(TOOL_DISTRIBUTIBLES) $(AVR_DISTRIBUTABLES) $(CONVERTED_IMAGE_H_FILES) $(CONVERTED_IMAGE_C_FILES)
 
 clean:
 	rm -rf dist obj
 
-obj/avr/compiled/%.o: src/software/%.c $(H_FILES) makefile
+%.aseprite.png: %.aseprite makefile
+	aseprite --batch $< --save-as $@
+
+%.jpg.h: %.jpg dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.jpg.c: %.jpg.h
+
+%.jpeg.h: %.jpeg dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.jpeg.c: %.jpeg.h
+
+%.png.h: %.png dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.png.c: %.png.h
+
+%.tga.h: %.tga dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.tga.c: %.tga.h
+
+%.bmp.h: %.bmp dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.bmp.c: %.bmp.h
+
+%.psd.h: %.psd dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.psd.c: %.psd.h
+
+%.gif.h: %.gif dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.gif.c: %.gif.h
+
+%.hdr.h: %.hdr dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.hdr.c: %.hdr.h
+
+%.pic.h: %.pic dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.pic.c: %.pic.h
+
+%.pnm.h: %.pnm dist/tools/image_to_sprite
+	./dist/tools/image_to_sprite \
+		$(shell realpath --relative-to $(dir $<) src/software/library/sprite.h) \
+		$@ \
+		$<.c \
+		$(basename $(basename $(basename $(notdir $@)))) \
+		`echo $(basename $(basename $(basename $(notdir $@)))) | tr a-z A-Z`_H \
+		< $<
+
+%.pnm.c: %.pnm.h
+
+dist/tools/%: src/software/tools/%.c $(SUBMODULE_H_FILES) makefile
 	mkdir -p $(dir $@)
+	rm -f $@
+	$(TOOL_CC) $(TOOL_CFLAGS) $< -o $@ $(TOOL_LDFLAGS)
+
+obj/avr/compiled/%.o: src/software/%.c $(APPLICATION_H_FILES) $(LIBRARY_H_FILES) $(TARGET_H_FILES) makefile
+	mkdir -p $(dir $@)
+	rm -f $@
 	$(AVR_CC) -c $(AVR_CFLAGS) $< -o $@
 
-obj/avr/linked/%.out: $(AVR_TARGET_AND_LIBRARY_O_FILES) $(AVR_APPLICATION_O_FILES)
+obj/avr/linked/%.out: $(AVR_TARGET_O_FILES) $(AVR_LIBRARY_O_FILES) $(AVR_APPLICATION_O_FILES) $(AVR_IMAGE_O_FILES)
 	mkdir -p $(dir $@)
-	$(AVR_CC) $(AVR_CFLAGS) $(AVR_TARGET_AND_LIBRARY_O_FILES) $(filter obj/avr/compiled/applications/$*/%,$(AVR_APPLICATION_O_FILES)) -o $@ $(AVR_LDFLAGS)
+	rm -f $@
+	$(AVR_CC) $(AVR_CFLAGS) $(AVR_TARGET_O_FILES) $(AVR_LIBRARY_O_FILES) $(filter obj/avr/compiled/applications/$*/%,$(AVR_APPLICATION_O_FILES)) $(filter obj/avr/compiled/applications/$*/%,$(AVR_IMAGE_O_FILES)) -o $@ $(AVR_LDFLAGS)
 
 dist/avr/%.hex: obj/avr/linked/%.out
 	mkdir -p $(dir $@)
+	rm -f $@
 	avr-objcopy -O ihex $< $@
---
+++ src/software/tools/image_to_sprite.c
@@ -0,0 +1,339 @@
+#include <stdint.h>
+#include <errno.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../../../submodules/nothings/stb/stb_image.h"
+
+#define QM_COLOR_TRANSPARENT 1
+#define QM_COLOR_BLACK 2
+#define QM_COLOR_WHITE 4
+
+typedef uint8_t qm_color_t;
+
+int main(int argc, const char ** argv) {
+  if (argc != 6) {
+    fprintf(stderr, "expected 5 arguments, actual %d\n", argc - 1);
+    return 1;
+  } else {
+    const char * path_to_library_header = argv[1];
+    const char * path_to_output_header = argv[2];
+    const char * path_to_output_source = argv[3];
+    const char * variable_name = argv[4];
+    const char * guard_name = argv[5];
+
+    int uncropped_columns;
+    int uncropped_rows;
+
+    uint8_t * uncropped_rgba = stbi_load_from_file(stdin, &uncropped_columns, &uncropped_rows, NULL, 4);
+
+    if (uncropped_rgba) {
+      int left_transparent_columns;
+
+      for (left_transparent_columns = 0; left_transparent_columns < uncropped_columns; left_transparent_columns++) {
+        int row;
+
+        for (row = 0; row < uncropped_rows; row++) {
+          if (uncropped_rgba[row * uncropped_columns * 4 + left_transparent_columns * 4 + 3]) {
+            break;
+          }
+        }
+
+        if (row < uncropped_rows) {
+          break;
+        }
+      }
+
+      if (left_transparent_columns == uncropped_columns) {
+        fprintf(stderr, "all pixels in the input file are transparent\n");
+        return 1;
+      } else if (left_transparent_columns > 127) {
+        fprintf(stderr, "the left %d columns are transparent (the limit being 127)\n", left_transparent_columns);
+        return 1;
+      } else {
+        int right_transparent_columns;
+
+        for (right_transparent_columns = 0; ; right_transparent_columns++) {
+          int row;
+
+          for (row = 0; row < uncropped_rows; row++) {
+            if (uncropped_rgba[row * uncropped_columns * 4 + (uncropped_columns - right_transparent_columns - 1) * 4 + 3]) {
+              break;
+            }
+          }
+
+          if (row < uncropped_rows) {
+            break;
+          }
+        }
+
+        int cropped_columns = uncropped_columns - left_transparent_columns - right_transparent_columns;
+
+        int rounded_up_cropped_columns = cropped_columns - (cropped_columns % 8) + 8;
+
+        if (rounded_up_cropped_columns > 255) {
+          fprintf(stderr, "the cropped area is %d columns wide when rounded up to the nearest 8 columns (the limit being 255)\n", cropped_columns);
+          return 1;
+        } else {
+          int top_transparent_rows;
+
+          for (top_transparent_rows = 0; ; top_transparent_rows++) {
+            int column;
+
+            for (column = left_transparent_columns; column < uncropped_columns - right_transparent_columns; column++) {
+              if (uncropped_rgba[top_transparent_rows * uncropped_columns * 4 + column * 4 + 3]) {
+                break;
+              }
+            }
+
+            if (column < uncropped_columns - right_transparent_columns) {
+              break;
+            }
+          }
+
+          if (top_transparent_rows > 127) {
+            fprintf(stderr, "the top %d rows are transparent (the limit being 127)\n", top_transparent_rows);
+            return 1;
+          } else {
+            int bottom_transparent_rows;
+
+            for (bottom_transparent_rows = 0; ; bottom_transparent_rows++) {
+              int column;
+
+              for (column = left_transparent_columns; column < uncropped_columns - right_transparent_columns; column++) {
+                if (uncropped_rgba[(uncropped_rows - bottom_transparent_rows - 1) * uncropped_columns * 4 + column * 4 + 3]) {
+                  break;
+                }
+              }
+
+              if (column < uncropped_columns - right_transparent_columns) {
+                break;
+              }
+            }
+
+            int cropped_rows = uncropped_rows - top_transparent_rows - bottom_transparent_rows;
+
+            if (cropped_rows > 255) {
+              fprintf(stderr, "the cropped area is %d rows tall (the limit being 255)\n", cropped_rows);
+              return 1;
+            } else {
+              qm_color_t * cropped_colors = (qm_color_t *) malloc(sizeof(qm_color_t) * rounded_up_cropped_columns * cropped_rows);
+
+              if (cropped_colors) {
+                qm_color_t encountered_colors = 0;
+
+                int cropped_index = 0;
+
+                for (int cropped_row = 0; cropped_row < cropped_rows; cropped_row++) {
+                  int uncropped_row = cropped_row + top_transparent_rows;
+
+                  for (int cropped_column = 0; cropped_column < rounded_up_cropped_columns; cropped_column++) {
+                    qm_color_t this_color;
+
+                    if (cropped_column < cropped_columns) {
+                      int uncropped_column = cropped_column + left_transparent_columns;
+
+                      int uncropped_index = uncropped_row * uncropped_columns * 4 + uncropped_column * 4;
+
+
+
+                      switch (uncropped_rgba[uncropped_index + 3]) {
+                        case 0:
+                          this_color = QM_COLOR_TRANSPARENT;
+                          break;
+
+                        case 255: {
+                          uint8_t red = uncropped_rgba[uncropped_index];
+                          uint8_t green = uncropped_rgba[uncropped_index + 1];
+                          uint8_t blue = uncropped_rgba[uncropped_index + 2];
+
+                          if (red == 0 && green == 0 && blue == 0) {
+                            this_color = QM_COLOR_BLACK;
+                            break;
+                          } else if (red == 255 && green == 255 && blue == 255) {
+                            this_color = QM_COLOR_WHITE;
+                            break;
+                          } else {
+                            fprintf(stderr, "the pixel at column %d, row %d is not transparent, black or white\n", uncropped_column, uncropped_row);
+                            return 1;
+                          }
+                        }
+
+                        default:
+                          fprintf(stderr, "the pixel at column %d, row %d is semi-transparent\n", uncropped_column, uncropped_row);
+                          return 1;
+                      }
+
+                      encountered_colors |= this_color;
+                    } else {
+                      this_color = QM_COLOR_TRANSPARENT;
+                    }
+
+                    cropped_colors[cropped_index] = this_color;
+                    cropped_index++;
+                  }
+                }
+
+                FILE * header_file = fopen(path_to_output_header, "w");
+
+                if (header_file) {
+                  if (fprintf(header_file, "#ifndef %s\n\n#define %s\n\n#include \"%s\"\n\nextern const qm_sprite_t %s QM_STATIC_DATA;\n\n#endif\n", guard_name, guard_name, path_to_library_header, variable_name) < 0) {
+                      fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_header, ferror(header_file));
+                      return 1;
+                  } else {
+                    FILE * source_file = fopen(path_to_output_source, "w");
+
+                    if (source_file) {
+                      if (fprintf(source_file, "#include \"%s\"\n#include \"%s.h\"\n\nconst qm_sprite_t %s QM_STATIC_DATA = {\n  %d,\n  %d,\n  %d,\n  %d\n  ", path_to_library_header, variable_name, variable_name, cropped_columns, cropped_rows, left_transparent_columns, top_transparent_rows) < 0) {
+                          fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                          return 1;
+                      } else {
+                        switch (encountered_colors) {
+                          case QM_COLOR_BLACK:
+                            if (fprintf(source_file, "QM_SPRITE_ENCODING_BLACK,\n}\n") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                            } else {
+                              return 0;
+                            }
+
+                          case QM_COLOR_WHITE:
+                            if (fprintf(source_file, "QM_SPRITE_ENCODING_WHITE,\n}\n") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                            } else {
+                              return 0;
+                            }
+
+                          case QM_COLOR_BLACK | QM_COLOR_WHITE:
+                            if (fprintf(source_file, "QM_SPRITE_ENCODING_BLACK_AND_WHITE,\n}\n") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                            } else {
+                              return 0;
+                            }
+
+                          case QM_COLOR_BLACK | QM_COLOR_TRANSPARENT:
+                            if (fprintf(source_file, "QM_SPRITE_ENCODING_BLACK_AND_TRANSPARENT,\n}\n") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                            } else {
+                              return 0;
+                            }
+
+                          case QM_COLOR_WHITE | QM_COLOR_TRANSPARENT:
+                            if (fprintf(source_file, "QM_SPRITE_ENCODING_WHITE_AND_TRANSPARENT,\n}\n") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                            } else {
+                              return 0;
+                            }
+
+                          case QM_COLOR_TRANSPARENT | QM_COLOR_BLACK | QM_COLOR_WHITE:
+                            if (fprintf(source_file, "QM_SPRITE_ENCODING_BLACK_WHITE_AND_TRANSPARENT,") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                            } else {
+                              for (int cropped_row = 0; cropped_row < cropped_rows; cropped_row++) {
+                                if (fprintf(source_file, "\n ") < 0) {
+                                  fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                  return 1;
+                                }
+
+                                for (int cropped_column = 0; cropped_column < rounded_up_cropped_columns; cropped_column += 8) {
+                                  if (fprintf(source_file, " 0b") < 0) {
+                                    fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                    return 1;
+                                  } else {
+                                    for (int bit_column = cropped_column; bit_column < cropped_column + 8; bit_column++) {
+                                      int character;
+
+                                      switch (cropped_colors[bit_column + cropped_row * rounded_up_cropped_columns]) {
+                                        case QM_COLOR_TRANSPARENT:
+                                          character = '1';
+                                          break;
+
+                                        case QM_COLOR_BLACK:
+                                        case QM_COLOR_WHITE:
+                                          character = '0';
+                                          break;
+
+                                        default:
+                                          fprintf(stderr, "unexpected color in cropped buffer\n");
+                                          return 1;
+                                      }
+
+                                      if (fputc(character, source_file) == EOF) {
+                                        fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                        return 1;
+                                      }
+                                    }
+
+                                    if (fprintf(source_file, ", 0b") < 0) {
+                                      fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                      return 1;
+                                    } else {
+                                      for (int bit_column = cropped_column; bit_column < cropped_column + 8; bit_column++) {
+                                        int character;
+
+                                        switch (cropped_colors[bit_column + cropped_row * rounded_up_cropped_columns]) {
+                                          case QM_COLOR_TRANSPARENT:
+                                          case QM_COLOR_BLACK:
+                                            character = '0';
+                                            break;
+
+                                          case QM_COLOR_WHITE:
+                                            character = '1';
+                                            break;
+
+                                          default:
+                                            fprintf(stderr, "unexpected color in cropped buffer\n");
+                                            return 1;
+                                        }
+
+                                        if (fputc(character, source_file) == EOF) {
+                                          fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                          return 1;
+                                        }
+                                      }
+
+                                      if (fputc(',', source_file) == EOF) {
+                                        fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                        return 1;
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+
+                              if (fprintf(source_file, "\n};\n") < 0) {
+                                fprintf(stderr, "failed to write to %s; error no %d\n", path_to_output_source, ferror(source_file));
+                                return 1;
+                              } else {
+                                return 0;
+                              }
+                            }
+                        }
+                      }
+                    } else {
+                      fprintf(stderr, "failed to open %s for writing; error no %d\n", path_to_output_header, errno);
+                      return 1;
+                    }
+                  }
+                } else {
+                  fprintf(stderr, "failed to open %s for writing; error no %d\n", path_to_output_header, errno);
+                  return 1;
+                }
+              } else {
+                fprintf(stderr, "failed to allocate memory for a cropped image\n");
+                return 1;
+              }
+            }
+          }
+        }
+      }
+    } else {
+      fprintf(stderr, "failed to load image; %s\n", stbi_failure_reason());
+      return 1;
+    }
+  }
+}
