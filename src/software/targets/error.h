5e25f65cb4a997b4c42977fde7a2d92c69ea236e
jameswilddev <jameswilddev@outlook.com>
2022-01-28 20:11:18 +0000

    Add a header for errors.

---
+++ src/software/targets/error.h
@@ -0,0 +1,11 @@
+#ifndef QM_ERROR_H
+
+#define QM_ERROR_H
+
+/**
+ * @brief Stops the application as a critical error has occurred.
+ * @param message A description of the critical error.
+ */
+extern void qm_error(const char * message);
+
+#endif
