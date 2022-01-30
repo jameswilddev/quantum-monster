8c6c7542327bac0a7674a34ac71b6f402b6d13a9
jameswilddev <jameswilddev@outlook.com>
2022-01-30 21:57:27 +0000

    Add helper to round up.

--- src/software/library/math.h
+++ src/software/library/math.h
@@ -18,4 +18,12 @@
  */
 #define QM_MATH_MAX(a, b) ((a) > (b) ? (a) : (b))
 
+/**
+ * @brief Rounds a given integer up to the nearest integer multiple of a given integer.
+ * @param value The integer to round up.
+ * @param to_nearest The integer to which to round.
+ * @return T The given integer, rounded up to the nearest integer multiple of the given integer.
+ */
+#define QM_MATH_ROUND_UP(value, to_nearest) ((((value) + ((to_nearest) - 1)) / (to_nearest)) * (to_nearest))
+
 #endif
